99a462d9c0d8ef60be1b6e32befc8646bc042092
Vinayak Raj Ranjan <vinayakrajqaz@gmail.com>
2024-01-26 23:21:30 +0530

    DataBase Connect

--- Backend/index.js
+++ Backend/index.js
@@ -1,6 +1,19 @@
-const express=require('express')
+const express=require('express');
+const { mongoose } = require('mongoose');
+
 
 const app=express();
+
+
+const db_link="mongodb+srv://vinayakrajqaz:iQkUnxrdkVaUsDQS@cluster0.wqnula8.mongodb.net/?retryWrites=true&w=majority"
+mongoose.connect(db_link)
+.then(function (db){
+    console.log("db Connected")
+
+})
+.catch(function(err){
+    console.log(err)
+})
 
 app.listen(3000)
 
