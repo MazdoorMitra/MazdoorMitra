c891843e04ee8f332928be0a996d189809ba41e2
Vinayak Raj Ranjan <vinayakrajqaz@gmail.com>
2024-01-27 17:03:08 +0530

    Login_And_Signup_Backend_api

---
+++ Backend/controllers/userController.js
@@ -0,0 +1,86 @@
+const userModel=require("../models/user")
+const bcrypt=require('bcrypt')
+const jwt=require("jsonwebtoken")
+const SECRET_KEY="DKJFHJDFKGHDFKJHSKDHFKJSFJK"
+
+const signup = async (req, res) => {
+    // existing user check
+    // hashed Password
+    // User creation
+    // Token Generate
+    const { username, number, password } = req.body;
+    try {
+        const existingUser = await userModel.findOne({ number: number });
+        if (existingUser) {
+            return res.status(400).json({ message: "User already exists" });
+        }
+
+        const hashPassword = await bcrypt.hash(password, 10);
+        const result = await userModel.create({
+            number: number,
+            password: hashPassword,
+            username: username,
+        });
+
+        const token = jwt.sign(
+            { number: result.number, id: result._id },
+            SECRET_KEY
+        );
+
+        res.status(201).json({ user: result, token: token });
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ message: "Something Went wrong" });
+    }
+};
+
+
+// const signin=async(req,res)=>{
+//     const {email,password}=req.body;
+//     try {
+//         const existingUser=await userModel.findOne({email:email});
+//         if(!existingUser){
+//             return res.status(404).json({message:"user Not Found"});
+
+//         }  
+//         const matchPassword=await bcrypt.compare(password,existingUser.password)
+//         if(!matchPassword){
+//             return res.status(400).json({message:"Invalid Credentials"});
+
+//         }
+//         const token=jwt.sign({email:existingUser.email,id:existingUser._id,SECRET_KEY})
+//         res.status(201).json({user:existingUser,token:token})
+
+//     } catch (error) {
+//         console.log(error)
+//         res.status(500).json({message:"Something Went wrong"});
+        
+//     }
+const signin = async (req, res) => {
+    const { number, password } = req.body;
+    try {
+        const existingUser = await userModel.findOne({ number: number });
+        if (!existingUser) {
+            return res.status(404).json({ message: "User Not Found" });
+        }
+
+        const matchPassword = await bcrypt.compare(password, existingUser.password);
+        if (!matchPassword) {
+            return res.status(400).json({ message: "Invalid Credentials" });
+        }
+
+        const token = jwt.sign(
+            { number: existingUser.number, id: existingUser._id },
+            SECRET_KEY
+        );
+
+        res.status(201).json({ user: existingUser, token: token });
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ message: "Something Went wrong" });
+    }
+};
+
+
+module.exports={signin,signup}
+
--- Backend/index.js
+++ Backend/index.js
@@ -1,9 +1,9 @@
 const express=require('express');
 const { mongoose } = require('mongoose');
+const userRouter = require('./routes/userRoutes');
 
 
 const app=express();
-
 const db_link="mongodb+srv://<username>:<password>@cluster0.wqnula8.mongodb.net/?retryWrites=true&w=majority"
 mongoose.connect(db_link)
 .then(function (db){
@@ -14,8 +14,12 @@ mongoose.connect(db_link)
     console.log(err)
 })
 
-app.listen(3000)
+//Middleware
+app.use(express.json());
+app.use("/users",userRouter);
+
 
 app.get('/',(req,res)=>{
     res.send('<h1>Hello <h1>')
 })
+app.listen(3000)
