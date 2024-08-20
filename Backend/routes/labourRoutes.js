beac4b734121341d93b1f97dec87d3df66b28bf1
Vinayak Raj Ranjan <vinayakrajqaz@gmail.com>
2024-08-20 16:54:59 +0530

    add code
    Signed-off-by: Vinayak Raj Ranjan <vinayakrajqaz@gmail.com>

--- Backend/routes/labourRoutes.js
+++ Backend/routes/labourRoutes.js
@@ -1,41 +1,4 @@
-// // routes/labourerRoutes.js
-// const express = require('express');
-// const router = express.Router();
-// const Contractor = require('../models/contractor');
 
-
-// // Define route for verifying labourer
-// router.post('/login', async (req, res) => {
-//     const { labourerId } = req.body;
-  
-//     try {
-//       // Find the contractor who has the labourer with the given ID
-//       const contractor = await Contractor.findOne({
-//         'projects.labourers._id': labourerId // Query to check if labourer ID exists
-//       }, {
-//         'projects.$': 1 // Return only the matching project
-//       });
-  
-//       if (!contractor) {
-//         return res.status(404).json({ message: 'Labourer not found' });
-//       }
-  
-//       const project = contractor.projects[0];
-//       const labourer = project.labourers.id(labourerId);
-  
-//       if (labourer) {
-//         res.status(200).json({ message: 'Login successful', labourer });
-//       } else {
-//         res.status(404).json({ message: 'Labourer not found' });
-//       }
-  
-//     } catch (error) {
-//       console.error('Error logging in:', error);
-//       res.status(500).json({ message: 'Server error' });
-//     }
-//   });
-
-// module.exports = router;
 const express = require('express');
 const router = express.Router();
 const Contractor = require('../models/contractor');
